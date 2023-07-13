844d31c075b2f12ee81b1d03e2b35e9d6adfe29e
小猪之最 <59649644+TmallKing1@users.noreply.github.com>
2023-07-13 11:03:51 +0800

    Version 1.1.0

--- src/main/java/top/pigest/scoreboardhelper/config/ScoreboardHelperConfigScreen.java
+++ src/main/java/top/pigest/scoreboardhelper/config/ScoreboardHelperConfigScreen.java
@@ -5,7 +5,10 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.gui.widget.SliderWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.option.SimpleOption;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
@@ -17,6 +20,7 @@ public class ScoreboardHelperConfigScreen extends Screen {
     private final Screen parent;
     private final ScoreboardHelperConfig config;
     private final List<TextWithShadow> list = new ArrayList<>();
+    private final int TITLE_Y = 30;
 
     public ScoreboardHelperConfigScreen(Screen parent, ScoreboardHelperConfig config) {
         super(Text.translatable(getTranslationKey("title", TranslationKeyType.NORMAL)));
@@ -26,9 +30,10 @@ public class ScoreboardHelperConfigScreen extends Screen {
 
     @Override
     protected void init() {
-        addDrawableChild(createBooleanPropertyButton(width / 2 - 100, height / 2 - 10 - 30, 200, 20, config.scoreboardShown));
-        addDrawableChild(createBooleanPropertyButton(width / 2 - 100, height / 2 - 10, 200, 20, config.sidebarScoreShown));
-        addDrawableChild(createIntegerPropertyTextField( width / 2 - 100, height / 2 - 10 + 40, 200, 20, config.maxShowCount));
+        addDrawableChild(createBooleanPropertyButton(width / 2 - 100, TITLE_Y + 40, 200, 20, config.scoreboardShown));
+        addDrawableChild(createBooleanPropertyButton(width / 2 - 100, TITLE_Y + 40 + 30, 200, 20, config.sidebarScoreShown));
+        addDrawableChild(createEnumPropertyButton(width / 2 - 100, TITLE_Y + 40 + 30 + 30, 200, 20, config.sidebarPosition, ScoreboardHelperConfig.ScoreboardSidebarPosition.values()));
+        addDrawableChild(createIntegerPropertyTextField( width / 2 - 100, TITLE_Y + 40 + 30 + 30 + 40, 200, 20, config.maxShowCount));
 
         addDrawableChild(new ButtonWidget.Builder(ScreenTexts.DONE, button -> close()).size(200, 20).position(width / 2 - 100, height - 40).build());
     }
@@ -49,7 +54,7 @@ public class ScoreboardHelperConfigScreen extends Screen {
         for(TextWithShadow text: list) {
             context.drawTextWithShadow(textRenderer, text.text, text.x, text.y, text.color);
         }
-        context.drawCenteredTextWithShadow(textRenderer, title, width / 2, 30, 0xFFFFFF);
+        context.drawCenteredTextWithShadow(textRenderer, title, width / 2, TITLE_Y, 0xFFFFFF);
         super.render(context, mouseX, mouseY, delta);
     }
 
@@ -79,6 +84,19 @@ public class ScoreboardHelperConfigScreen extends Screen {
             button.setMessage(ScreenTexts.composeToggleText(text, newValue));
             property.setValue(newValue);
         }).tooltip(Tooltip.of(toolTip)).size(width, height).position(x, y).build();
+    }
+
+    @SafeVarargs
+    private <T extends Enum<?>> CyclingButtonWidget<T> createEnumPropertyButton(int x, int y, int width, int height, Property<T> property, T ... values) {
+        Text text = Text.translatable(getTranslationKey(property.getKey(), TranslationKeyType.NORMAL));
+        Text tooltip = Text.translatable(getTranslationKey(property.getKey(), TranslationKeyType.TOOLTIP));
+        CyclingButtonWidget<T> positionCyclingButtonWidget = CyclingButtonWidget.<T>builder(value -> Text.translatable(getTranslationKey(property.getKey(),
+                TranslationKeyType.NORMAL) + ".value." + value.toString())).values(values).initially(property.getValue())
+                .build(x, y, width, height, text, (button, value) -> {
+                    property.setValue(value);
+                });
+        positionCyclingButtonWidget.setTooltip(Tooltip.of(tooltip));
+        return positionCyclingButtonWidget;
     }
 
     private TextFieldWidget createIntegerPropertyTextField(int x, int y, int width, int height, Property<Integer> property) {
--- src/main/java/top/pigest/scoreboardhelper/mixin/InGameHudMixin.java
+++ src/main/java/top/pigest/scoreboardhelper/mixin/InGameHudMixin.java
@@ -1,24 +1,38 @@
 package top.pigest.scoreboardhelper.mixin;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import top.pigest.scoreboardhelper.config.ScoreboardHelperConfig;
 import top.pigest.scoreboardhelper.util.Constants;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
+    @Unique
+    private int j;
+
+    @Inject(method = "renderScoreboardSidebar", at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void injected0(DrawContext context, ScoreboardObjective objective, CallbackInfo ci, Scoreboard scoreboard, Collection<ScoreboardPlayerScore> collection, List list, List<Pair<ScoreboardPlayerScore, MutableText>> list2, Text text, int i, int j) {
+        this.j = j;
+    }
+
 
     @Inject(method = "renderScoreboardSidebar", at = @At(value = "HEAD"), cancellable = true)
     private void injected1(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
@@ -70,5 +84,31 @@ public abstract class InGameHudMixin {
         } else{
             return "";
         }
+    }
+
+    @ModifyVariable(method = "renderScoreboardSidebar", at = @At(value = "STORE"), ordinal = 10)
+    private int injected6(int s) {
+        switch (ScoreboardHelperConfig.INSTANCE.sidebarPosition.getValue()) {
+            case LEFT -> {
+                return 3;
+            }
+            case RIGHT -> {
+                return s;
+            }
+        }
+        return s;
+    }
+
+    @ModifyVariable(method = "renderScoreboardSidebar", at = @At(value = "STORE"), ordinal = 12)
+    private int injected7(int u) {
+        switch (ScoreboardHelperConfig.INSTANCE.sidebarPosition.getValue()) {
+            case LEFT -> {
+                return 3 + j + 2;
+            }
+            case RIGHT -> {
+                return u;
+            }
+        }
+        return u;
     }
 }
